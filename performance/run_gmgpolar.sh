39368cfe4630c922dca02f5970ed5e497401007e
Martin Kühn <Martin.Kuehn@DLR.de>
2023-03-27 13:21:14 +0200

    run script jz hackathon

--- performance/run_gmgpolar.sh
+++ performance/run_gmgpolar.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#SBATCH --job-name=Seq              # name of the job
+#SBATCH --nodes=1                   # number of nodes
+#SBATCH --ntasks-per-node=1         # number of MPI tasks per node
+#SBATCH --time=00:03:00             # maximum execution time requested (HH:MM:SS)
+#SBATCH --output=Seq%j.out          # name of output file
+#SBATCH --error=Seq%j.out           # name of error file (here, in common with the output)
+#SBATCH --account=emp@cpu
+# #SBATCH --partition=gpu_p2
+
+#cd ${SLURM_SUBMIT_DIR}
 
 #fixed variables
 origin_NOT_coarse=0	# origin_NOT_coarse
@@ -17,7 +27,7 @@ nr_exp=4		# n
 mod_pk=0		# mod_pk (0/1)
 R0=0.1			# r (1e-8/1e-5/1e-2)
 DirBC_Interior=0	#  DirBC_Interior (0/1)
-divideBy2=0		# divideBy2 (3/4/5/6)
+divideBy2=3		# divideBy2 (3/4/5/6)
 smoother=3		# smoother (3,13)
 extrapolation=0		# E
 
@@ -32,27 +42,25 @@ echo "#SBATCH --error=slurm-%A-p$prob-fa$fac_ani-r$nr_exp-mpk$mod_pk-s$smoother-
 echo "#SBATCH -N 1" >> run_gmgpolar_sbatch.sh
 echo "#SBATCH -n 1" >> run_gmgpolar_sbatch.sh
 echo "#SBATCH -c 14" >> run_gmgpolar_sbatch.sh
-echo "#SBATCH -t 6000" >> run_gmgpolar_sbatch.sh
+echo "#SBATCH -t 25" >> run_gmgpolar_sbatch.sh
 echo "#SBATCH --exclusive" >> run_gmgpolar_sbatch.sh
- 
+echo "#SBATCH --account=emp@cpu" >> run_gmgpolar_sbatch.sh
 # remove potentially loaded and conflicting modules
 echo "module purge" >> run_gmgpolar_sbatch.sh
 
 # gcc10
-echo "module load PrgEnv/gcc10-openmpi" >> run_gmgpolar_sbatch.sh
+echo "module load openmpi/4.1.1-cuda" >> run_gmgpolar_sbatch.sh
+echo "module load cmake" >> run_gmgpolar_sbatch.sh
 
-echo "cd .." >> run_gmgpolar_sbatch.sh
-echo "make -j16" >> run_gmgpolar_sbatch.sh
+#echo "export OMP_PLACES=cores" >> run_gmgpolar_sbatch.sh
+echo "export OMP_NUM_THREADS=14" >> run_gmgpolar_sbatch.sh
+
+echo "cd $SCRATCH/GMGPolar/build" >> run_gmgpolar_sbatch.sh
+echo "cmake --build . -- -j16" >> run_gmgpolar_sbatch.sh
 
 # FLOPS-DP counter
-echo "for m in {0..1}; do" >> run_gmgpolar_sbatch.sh
-echo "likwid-perfctr -C 0-$m -g FLOPS_DP ./build_gnu/main --matrix_free 1 -n $nr_exp -a $fac_ani --mod_pk $mod_pk --DirBC_Interior $DirBC_Interior --divideBy2 $divideBy2 -r $R0  --smoother $smoother -E $extrapolation" >> run_gmgpolar_sbatch.sh
-echo "done;" >> run_gmgpolar_sbatch.sh
-
-# memory (saturation) benchmarks
-echo "for m in {0..1}; do" >> run_gmgpolar_sbatch.sh
-echo "likwid-perfctr -C 0-$m -g CACHES ./build_gnu/main -n $nr_exp -a $fac_ani --mod_pk $mod_pk --DirBC_Interior $DirBC_Interior --divideBy2 $divideBy2 -r $R0  --smoother $smoother -E $extrapol" >> run_gmgpolar_sbatch.sh
-echo "done;" >> run_gmgpolar_sbatch.sh
+echo "./gmgpolar_simulation --openmp 14  --matrix_free 1 -n $nr_exp -a $fac_ani --mod_pk $mod_pk --DirBC_Interior $DirBC_Interior --divideBy2 $divideBy2 -r $R0  --smoother $smoother -E $extrapolation" >> run_gmgpolar_sbatch.sh
 
 #submit the job
 sbatch run_gmgpolar_sbatch.sh
+
