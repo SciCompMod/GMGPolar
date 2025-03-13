daac90e0fa85de0f70477798a714a22af2eb50ec
Litz <litz_ju@fe-store02.maas>
2025-03-13 11:50:14 +0100

    Stencil Comment: Implemented suggested changes from review (discussion_r1900728638)

--- include/Stencil/stencil.h
+++ include/Stencil/stencil.h
@@ -16,6 +16,19 @@ enum class StencilPosition
     BottomRight,
 };
 
+/**
+ * @brief Represents a stencil pattern used in sparse matrix construction.
+ *
+ * The Stencil class helps define neighborhood structures, typically for 
+ * discretized numerical methods such as finite difference or finite element methods.
+ * It maps each `StencilPosition` to an integer index that represents its 
+ * inclusion in the stencil pattern.
+ *
+ * Non-zero indices are obtained via `ptr + offset`, where:
+ * - The Stencil class stores the offset for each position.
+ * - A value of `-1` means the position is not included in the stencil pattern.
+ * - Other values (0, 1, 2, ..., stencil_size - 1) correspond to valid stencil indices.
+ */
 class Stencil
 {
 public:
