982f625b05f31a8d3255f3563269b47b406decdb
Julian Litz <91479202+julianlitz@users.noreply.github.com>
2025-08-20 12:37:37 +0200

    Update run_gmgpolar_2.0.sh

--- scripts/comparison/run_gmgpolar_2.0.sh
+++ scripts/comparison/run_gmgpolar_2.0.sh
@@ -138,4 +138,37 @@ else
     exit 1
 fi
 
-./../../build/gmgpolar --verbose $verbose --paraview $paraview --maxOpenMPThreads $maxOpenMPThreads --threadReductionFactor $threadReductionFactor --stencilDistributionMethod $stencilDistributionMethod --cacheDensityProfileCoefficients $cacheDensityProfileCoefficients --cacheDomainGeometry $cacheDomainGeometry --R0 $R0 --Rmax $Rmax --nr_exp $nr_exp --ntheta_exp $ntheta_exp --anisotropic_factor $anisotropic_factor --divideBy2 $divideBy2 --DirBC_Interior $DirBC_Interior --geometry $geometry --kappa_eps $kappa_eps --delta_e $delta_e --problem $problem --alpha_coeff $alpha_coeff --alpha_jump $alpha_jump --beta_coeff $beta_coeff --FMG $FMG --FMG_iterations $FMG_iterations --FMG_cycle $FMG_cycle --extrapolation $extrapolation --maxLevels $maxLevels --preSmoothingSteps $preSmoothingSteps --postSmoothingSteps $postSmoothingSteps --multigridCycle $multigridCycle --maxIterations $maxIterations --residualNormType $residualNormType --absoluteTolerance $absoluteTolerance --relativeTolerance $relativeTolerance
+./../../build/gmgpolar \
+    --verbose $verbose \
+    --paraview $paraview \
+    --maxOpenMPThreads $maxOpenMPThreads \
+    --threadReductionFactor $threadReductionFactor \
+    --stencilDistributionMethod $stencilDistributionMethod \
+    --cacheDensityProfileCoefficients $cacheDensityProfileCoefficients \
+    --cacheDomainGeometry $cacheDomainGeometry \
+    --R0 $R0 \
+    --Rmax $Rmax \
+    --nr_exp $nr_exp \
+    --ntheta_exp $ntheta_exp \
+    --anisotropic_factor $anisotropic_factor \
+    --divideBy2 $divideBy2 \
+    --DirBC_Interior $DirBC_Interior \
+    --geometry $geometry \
+    --kappa_eps $kappa_eps \
+    --delta_e $delta_e \
+    --problem $problem \
+    --alpha_coeff $alpha_coeff \
+    --alpha_jump $alpha_jump \
+    --beta_coeff $beta_coeff \
+    --FMG $FMG \
+    --FMG_iterations $FMG_iterations \
+    --FMG_cycle $FMG_cycle \
+    --extrapolation $extrapolation \
+    --maxLevels $maxLevels \
+    --preSmoothingSteps $preSmoothingSteps \
+    --postSmoothingSteps $postSmoothingSteps \
+    --multigridCycle $multigridCycle \
+    --maxIterations $maxIterations \
+    --residualNormType $residualNormType \
+    --absoluteTolerance $absoluteTolerance \
+    --relativeTolerance $relativeTolerance
