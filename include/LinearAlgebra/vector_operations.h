4dd3582193d467fd6ff2dc5652b1fe4d789eaed7
KARA Abdelhadi 265943 <abdelhadi.kara@cea.fr>
2025-09-11 13:45:19 +0200

    use Vector alias for kokkos views in Linear Algebra header

--- include/LinearAlgebra/vector_operations.h
+++ include/LinearAlgebra/vector_operations.h
@@ -12,11 +12,11 @@ template <typename T>
 bool equals(const Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> lhs,
             const Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> rhs)
 {
-    if (lhs.size() != rhs.size()) {
+    if (lhs.extent(0) != rhs.extent(0)) {
         return false;
     }
 
-    const std::size_t n = lhs.size();
+    const std::size_t n = lhs.extent(0);
     for (std::size_t i = 0; i < n; ++i) {
         if (!equals(lhs[i], rhs[i])) {
             return false;
@@ -28,7 +28,7 @@ bool equals(const Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> lhs,
 template <typename T>
 void assign(Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> lhs, const T& value)
 {
-    std::size_t n = lhs.size();
+    std::size_t n = lhs.extent(0);
 #pragma omp parallel for if (n > 10'000)
     for (std::size_t i = 0; i < n; ++i) {
         lhs[i] = value;
@@ -39,13 +39,13 @@ template <typename T>
 void add(Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> result,
          const Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> x)
 {
-    if (result.size() != x.size()) {
+    if (result.extent(0) != x.extent(0)) {
         throw std::invalid_argument("Vectors must be of the same size.");
     }
-    std::size_t n = result.size();
+    std::size_t n = result.extent(0);
 #pragma omp parallel for if (n > 10'000)
     for (std::size_t i = 0; i < n; ++i) {
-        result[i] += x[i];
+        result[i] += x(i);
     }
 }
 
@@ -53,13 +53,13 @@ template <typename T>
 void subtract(Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> result,
               const Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> x)
 {
-    if (result.size() != x.size()) {
+    if (result.extent(0) != x.extent(0)) {
         throw std::invalid_argument("Vectors must be of the same size.");
     }
-    std::size_t n = result.size();
+    std::size_t n = result.extent(0);
 #pragma omp parallel for if (n > 10'000)
     for (std::size_t i = 0; i < n; ++i) {
-        result[i] -= x[i];
+        result[i] -= x(i);
     }
 }
 
@@ -67,23 +67,23 @@ template <typename T>
 void linear_combination(Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> x, const T& alpha,
                         const Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> y, const T& beta)
 {
-    if (x.size() != y.size()) {
+    if (x.extent(0) != y.extent(0)) {
         throw std::invalid_argument("Vectors must be of the same size.");
     }
-    std::size_t n = x.size();
+    std::size_t n = x.extent(0);
 #pragma omp parallel for if (n > 10'000)
     for (std::size_t i = 0; i < n; ++i) {
-        x[i] = alpha * x[i] + beta * y[i];
+        x(i) = alpha * x(i) + beta * y[i];
     }
 }
 
 template <typename T>
 void multiply(Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> x, const T& alpha)
 {
-    std::size_t n = x.size();
+    std::size_t n = x.extent(0);
 #pragma omp parallel for if (n > 10'000)
     for (std::size_t i = 0; i < n; ++i) {
-        x[i] *= alpha;
+        x(i) *= alpha;
     }
 }
 
@@ -91,12 +91,12 @@ template <typename T>
 T dot_product(const Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> lhs,
               const Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> rhs)
 {
-    if (lhs.size() != rhs.size()) {
+    if (lhs.extent(0) != rhs.extent(0)) {
         throw std::invalid_argument("Vectors must be of the same size.");
     }
 
     T result      = 0.0;
-    std::size_t n = lhs.size();
+    std::size_t n = lhs.extent(0);
 #pragma omp parallel for reduction(+ : result) if (n > 10'000)
     for (std::size_t i = 0; i < n; ++i) {
         result += lhs[i] * rhs[i];
@@ -108,10 +108,10 @@ template <typename T>
 T l1_norm(const Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> x)
 {
     T result      = 0.0;
-    std::size_t n = x.size();
+    std::size_t n = x.extent(0);
 #pragma omp parallel for reduction(+ : result) if (n > 10'000)
     for (std::size_t i = 0; i < n; ++i) {
-        result += std::abs(x[i]);
+        result += std::abs(x(i));
     }
     return result;
 }
@@ -120,10 +120,10 @@ template <typename T>
 T l2_norm_squared(const Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> x)
 {
     T result      = 0.0;
-    std::size_t n = x.size();
+    std::size_t n = x.extent(0);
 #pragma omp parallel for reduction(+ : result) if (n > 10'000)
     for (std::size_t i = 0; i < n; ++i) {
-        result += x[i] * x[i];
+        result += x(i) * x(i);
     }
     return result;
 }
@@ -131,12 +131,12 @@ T l2_norm_squared(const Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace>
 template <typename T>
 T l2_norm(const Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> x)
 {
-    const std::size_t n = x.size();
+    const std::size_t n = x.extent(0);
     // 1) find the largest absolute value
     T scale = 0.0;
 #pragma omp parallel for reduction(max : scale) if (n > 10'000)
     for (std::size_t i = 0; i < n; ++i) {
-        T abs_val = std::abs(x[i]);
+        T abs_val = std::abs(x(i));
         if (abs_val > scale) {
             scale = abs_val;
         }
@@ -145,21 +145,21 @@ T l2_norm(const Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> x)
     T sum = 0.0;
 #pragma omp parallel for reduction(+ : sum) if (n > 10'000)
     for (std::size_t i = 0; i < n; ++i) {
-        T value = x[i] / scale;
-        sum += value * value;
+        T value = x(i) / scale;
+        sum +=value * value;
     }
     // 3) rescale
-    return scale * std::sqrt(sum);
+    return  scale *std::sqrt(sum);
 }
 
 template <typename T>
 T infinity_norm(const Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> x)
 {
     T result      = 0.0;
-    std::size_t n = x.size();
+    std::size_t n = x.extent(0);
 #pragma omp parallel for reduction(max : result) if (n > 10'000)
     for (std::size_t i = 0; i < n; ++i) {
-        T abs_value = std::abs(x[i]);
+        T abs_value = std::abs(x(i));
         if (abs_value > result) {
             result = abs_value;
         }
