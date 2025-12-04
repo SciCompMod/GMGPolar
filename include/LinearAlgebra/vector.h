69863ad9214204573d39ab45c37aa7f42f5469c6
Emily Bourne <emily.bourne@epfl.ch>
2025-12-04 10:30:32 +0100

    Use Kokkos vectors in COO and CSR matrices

--- include/LinearAlgebra/vector.h
+++ include/LinearAlgebra/vector.h
@@ -10,6 +10,8 @@
 #include <Kokkos_Core.hpp>
 
 template <typename T>
+using AllocatableVector = Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace>;
+template <typename T>
 using Vector = Kokkos::View<T*, Kokkos::LayoutRight, Kokkos::HostSpace> const;
 template <typename T>
 using ConstVector = Kokkos::View<const T*, Kokkos::LayoutRight, Kokkos::HostSpace> const;
