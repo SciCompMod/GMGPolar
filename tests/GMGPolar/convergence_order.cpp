e1a57346588dc96eb94680c70f6aef57ddac36a9
Emily Bourne <emily.bourne@epfl.ch>
2025-12-17 15:41:15 +0100

    Ensure DensityProfileCoefficients type is available

--- tests/GMGPolar/convergence_order.cpp
+++ tests/GMGPolar/convergence_order.cpp
@@ -152,6 +152,7 @@ std::vector<double> refine(std::vector<double> const& original_points)
     return refined;
 }
 
+template<class DensityProfileCoefficients>
 std::tuple<double, double> get_gmgpolar_error(PolarGrid const& grid, CzarnyGeometry const& domain_geometry,
                                               DensityProfileCoefficients const& coefficients,
                                               BoundaryConditions const& boundary_conditions,
