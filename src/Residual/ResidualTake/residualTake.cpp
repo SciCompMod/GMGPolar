a8a4bce9374dc279b89293782e5e9dd9402c1c17
Julian Litz <91479202+julianlitz@users.noreply.github.com>
2026-01-27 15:15:43 +0100

    Improve readability of Residual implementation (#157)

--- src/Residual/ResidualTake/residualTake.cpp
+++ src/Residual/ResidualTake/residualTake.cpp
@@ -18,29 +18,18 @@ void ResidualTake::computeResidual(Vector<double> result, ConstVector<double> rh
     assert(level_cache_.cacheDensityProfileCoefficients());
     assert(level_cache_.cacheDomainGeometry());
 
-    if (num_omp_threads_ == 1) {
-        /* Single-threaded execution */
+    #pragma omp parallel num_threads(num_omp_threads_)
+    {
+        /* Circle Section */
+        #pragma omp for nowait
         for (int i_r = 0; i_r < grid_.numberSmootherCircles(); i_r++) {
             applyCircleSection(i_r, result, rhs, x);
         }
+        
+        /* Radial Section */
+        #pragma omp for nowait
         for (int i_theta = 0; i_theta < grid_.ntheta(); i_theta++) {
             applyRadialSection(i_theta, result, rhs, x);
-        }
-    }
-    else {
-        /* Multi-threaded execution */
-        #pragma omp parallel num_threads(num_omp_threads_)
-        {
-            /* Circle Section */
-            #pragma omp for nowait
-            for (int i_r = 0; i_r < grid_.numberSmootherCircles(); i_r++) {
-                applyCircleSection(i_r, result, rhs, x);
-            }
-            /* Radial Section */
-            #pragma omp for nowait
-            for (int i_theta = 0; i_theta < grid_.ntheta(); i_theta++) {
-                applyRadialSection(i_theta, result, rhs, x);
-            }
         }
     }
 }
