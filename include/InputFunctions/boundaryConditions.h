41b2aa68a6128dfe790943c47b2a2d71fb39970a
Emily Bourne <emily.bourne@epfl.ch>
2026-01-27 17:05:28 +0100

    Template IGMGPolar::build_rhs_f and IGMGPolar::solve on concepts::BoundaryConditions

--- include/InputFunctions/boundaryConditions.h
+++ include/InputFunctions/boundaryConditions.h
@@ -10,3 +10,14 @@ public:
     // Only used if DirBC_Interior = true
     virtual double u_D_Interior(double r, double theta) const = 0;
 };
+
+namespace concepts
+{
+
+template <typename T>
+concept BoundaryConditions = !std::same_as<T, BoundaryConditions> && requires(const T bcs, double r, double theta) {
+    { bcs.u_D(r, theta) } -> std::convertible_to<double>;
+    { bcs.u_D_Interior(r, theta) } -> std::convertible_to<double>;
+};
+
+} // namespace concepts
