e9d146390889a0973e039289f34c2e1e994e4ca4
Julian Litz <91479202+julianlitz@users.noreply.github.com>
2026-01-27 23:14:28 +0100

    Fix function signature for multigrid_W_Cycle

--- src/GMGPolar/MultigridMethods/multigrid_W_Cycle.cpp
+++ src/GMGPolar/MultigridMethods/multigrid_W_Cycle.cpp
@@ -1,6 +1,6 @@
 #include "../../../include/GMGPolar/gmgpolar.h"
 
-void IGMGPolar::multigrid_W_Cycle(const int level_depth, Vector<double> solution, Vector<double> rhs,
+void IGMGPolar::multigrid_W_Cycle(int level_depth, Vector<double> solution, Vector<double> rhs,
                                   Vector<double> residual)
 {
     assert(0 <= level_depth && level_depth < number_of_levels_ - 1);
