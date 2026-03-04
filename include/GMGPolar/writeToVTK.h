febdc2a85ec2dd556db6a0b41c9eb575b4c147fc
Emily Bourne <emily.bourne@epfl.ch>
2026-03-04 17:09:44 +0100

    Paramterise Level

--- include/GMGPolar/writeToVTK.h
+++ include/GMGPolar/writeToVTK.h
@@ -62,11 +62,11 @@ void GMGPolar<DomainGeometry, DensityProfileCoefficients>::writeToVTK(const std:
 
 template <concepts::DomainGeometry DomainGeometry, concepts::DensityProfileCoefficients DensityProfileCoefficients>
 void GMGPolar<DomainGeometry, DensityProfileCoefficients>::writeToVTK(const std::filesystem::path& file_path,
-                                                                      const Level& level,
+                                                                      const Level<DomainGeometry>& level,
                                                                       ConstVector<double> grid_function)
 {
-    const PolarGrid& grid         = level.grid();
-    const LevelCache& level_cache = level.levelCache();
+    const PolarGrid& grid                        = level.grid();
+    const LevelCache<DomainGeometry>& level_cache = level.levelCache();
 
     assert(std::ssize(grid_function) == grid.numberOfNodes());
 
