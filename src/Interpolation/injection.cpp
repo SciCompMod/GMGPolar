babd69753988d1d1e12fde25dd159d6b0328cd2e
julianlitz <julian.litz@icloud.com>
2026-05-01 00:08:19 +0200

    litz_002

--- src/Interpolation/injection.cpp
+++ src/Interpolation/injection.cpp
@@ -1,16 +1,19 @@
 #include "../../include/Interpolation/interpolation.h"
+
 using namespace gmgpolar;
 
 /* Remark: This injection is not scaled. */
-
 static inline void coarseNodeInjection(int i_r_coarse, int i_theta_coarse, const PolarGrid& fine_grid,
                                        const PolarGrid& coarse_grid, Vector<double>& coarse_result,
                                        ConstVector<double>& fine_values)
 {
-    int i_r     = i_r_coarse * 2;
-    int i_theta = i_theta_coarse * 2;
+    const int i_r     = i_r_coarse * 2;
+    const int i_theta = i_theta_coarse * 2;
 
-    coarse_result[coarse_grid.index(i_r_coarse, i_theta_coarse)] = fine_values[fine_grid.index(i_r, i_theta)];
+    const int coarse_index = coarse_grid.index(i_r_coarse, i_theta_coarse);
+    const int fine_index   = fine_grid.index(i_r, i_theta);
+
+    coarse_result[coarse_index] = fine_values[fine_grid.index(i_r, i_theta)];
 }
 
 void Interpolation::applyInjection(const PolarGrid& fine_grid, const PolarGrid& coarse_grid,
@@ -19,22 +22,21 @@ void Interpolation::applyInjection(const PolarGrid& fine_grid, const PolarGrid& 
     assert(std::ssize(fine_values) == fine_grid.numberOfNodes());
     assert(std::ssize(coarse_result) == coarse_grid.numberOfNodes());
 
-#pragma omp parallel num_threads(max_omp_threads_)
-    {
-/* For loop matches circular access pattern */
-#pragma omp for nowait
-        for (int i_r_coarse = 0; i_r_coarse < coarse_grid.numberSmootherCircles(); i_r_coarse++) {
-            for (int i_theta_coarse = 0; i_theta_coarse < coarse_grid.ntheta(); i_theta_coarse++) {
-                coarseNodeInjection(i_r_coarse, i_theta_coarse, fine_grid, coarse_grid, coarse_result, fine_values);
-            }
-        }
+    const int smoother_circles = coarse_grid.numberSmootherCircles();
+    const int ntheta           = coarse_grid.ntheta();
+    const int nr               = coarse_grid.nr();
 
-/* For loop matches radial access pattern */
-#pragma omp for nowait
-        for (int i_theta_coarse = 0; i_theta_coarse < coarse_grid.ntheta(); i_theta_coarse++) {
-            for (int i_r_coarse = coarse_grid.numberSmootherCircles(); i_r_coarse < coarse_grid.nr(); i_r_coarse++) {
-                coarseNodeInjection(i_r_coarse, i_theta_coarse, fine_grid, coarse_grid, coarse_result, fine_values);
-            }
-        }
-    }
+    /* For loop matches circular access pattern */
+    Kokkos::parallel_for(
+        "injection_circular", Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0, 0}, {smoother_circles, ntheta}),
+        KOKKOS_LAMBDA(int i_r_coarse, int i_theta_coarse) {
+            coarseNodeInjection(i_r_coarse, i_theta_coarse, fine_grid, coarse_grid, coarse_result, fine_values);
+        });
+
+    /* For loop matches radial access pattern */
+    Kokkos::parallel_for(
+        "injection_radial", Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0, smoother_circles}, {ntheta, nr}),
+        KOKKOS_LAMBDA(int i_theta_coarse, int i_r_coarse) {
+            coarseNodeInjection(i_r_coarse, i_theta_coarse, fine_grid, coarse_grid, coarse_result, fine_values);
+        });
 }
