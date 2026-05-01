f4ec9f3164c3ab8d4c014c7923bf31dfb4180e98
julianlitz <julian.litz@icloud.com>
2026-05-01 18:48:47 +0200

    Use Kokkos::parallel_for

--- src/Interpolation/injection.cpp
+++ src/Interpolation/injection.cpp
@@ -22,11 +22,13 @@ void Interpolation::applyInjection(const PolarGrid& fine_grid, const PolarGrid& 
     assert(std::ssize(fine_values) == fine_grid.numberOfNodes());
     assert(std::ssize(coarse_result) == coarse_grid.numberOfNodes());
 
+    /* We split the loops into two regions to better respect the */
+    /* access patterns of the smoother and improve cache locality. */
+
     // The For loop matches circular access pattern */
     Kokkos::parallel_for(
         "Interpolation: Injection (Circular)",
-        // Rank of the index space, Iteration pattern between tiles, Iteration pattern within tiles
-        Kokkos::MDRangePolicy<Kokkos::Rank<2, Kokkos::Iterate::Right, Kokkos::Iterate::Right>>( // Iteration policy
+        Kokkos::MDRangePolicy<Kokkos::Rank<2>>( // Rank of the index space
             {0, 0}, // Starting point of the index space
             {coarse_grid.numberSmootherCircles(), coarse_grid.ntheta()} // Ending point of the index space
             ),
@@ -38,13 +40,12 @@ void Interpolation::applyInjection(const PolarGrid& fine_grid, const PolarGrid& 
     /* For loop matches radial access pattern */
     Kokkos::parallel_for(
         "Interpolation: Injection (Radial)",
-        // Rank of the index space, Iteration pattern between tiles, Iteration pattern within tiles
-        Kokkos::MDRangePolicy<Kokkos::Rank<2, Kokkos::Iterate::Left, Kokkos::Iterate::Left>>( // Iteration policy
-            {coarse_grid.numberSmootherCircles(), 0}, // Starting point of the index space
-            {coarse_grid.nr(), coarse_grid.ntheta()} // Ending point of the index space
+        Kokkos::MDRangePolicy<Kokkos::Rank<2>>( // Rank of the index space
+            {0, coarse_grid.numberSmootherCircles()}, // Starting point of the index space
+            {coarse_grid.ntheta(), coarse_grid.nr()} // Ending point of the index space
             ),
         // Kokkos lambda function to execute for each point in the index space
-        KOKKOS_LAMBDA(const int i_r_coarse, const int i_theta_coarse) {
+        KOKKOS_LAMBDA(const int i_theta_coarse, const int i_r_coarse) {
             coarseNodeInjection(i_r_coarse, i_theta_coarse, fine_grid, coarse_grid, coarse_result, fine_values);
         });
 
