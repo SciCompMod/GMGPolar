c7baa46288c8d9e7f17070f85d0e21eef4ef3bce
Julian Litz <91479202+julianlitz@users.noreply.github.com>
2026-05-01 12:46:27 +0200

    const int

--- src/Interpolation/injection.cpp
+++ src/Interpolation/injection.cpp
@@ -3,7 +3,7 @@
 using namespace gmgpolar;
 
 /* Remark: This injection is not scaled. */
-static inline void coarseNodeInjection(int i_r_coarse, int i_theta_coarse, const PolarGrid& fine_grid,
+static inline void coarseNodeInjection(const int i_r_coarse, const int i_theta_coarse, const PolarGrid& fine_grid,
                                        const PolarGrid& coarse_grid, Vector<double>& coarse_result,
                                        ConstVector<double>& fine_values)
 {
@@ -22,20 +22,16 @@ void Interpolation::applyInjection(const PolarGrid& fine_grid, const PolarGrid& 
     assert(std::ssize(fine_values) == fine_grid.numberOfNodes());
     assert(std::ssize(coarse_result) == coarse_grid.numberOfNodes());
 
-    const int smoother_circles = coarse_grid.numberSmootherCircles();
-    const int ntheta           = coarse_grid.ntheta();
-    const int nr               = coarse_grid.nr();
-
     // The For loop matches circular access pattern */
     Kokkos::parallel_for(
         "Interpolation: Injection (Circular)",
         // Rank of the index space, Iteration pattern between tiles, Iteration pattern within tiles
         Kokkos::MDRangePolicy<Kokkos::Rank<2, Kokkos::Iterate::Right, Kokkos::Iterate::Right>>( // Iteration policy
             {0, 0}, // Starting point of the index space
-            {smoother_circles, ntheta} // Ending point of the index space
+            {coarse_grid.numberSmootherCircles(), coarse_grid.ntheta()} // Ending point of the index space
             ),
         // Kokkos lambda function to execute for each point in the index space
-        KOKKOS_LAMBDA(int i_r_coarse, int i_theta_coarse) {
+        KOKKOS_LAMBDA(const int i_r_coarse, const int i_theta_coarse) {
             coarseNodeInjection(i_r_coarse, i_theta_coarse, fine_grid, coarse_grid, coarse_result, fine_values);
         });
 
@@ -44,11 +40,11 @@ void Interpolation::applyInjection(const PolarGrid& fine_grid, const PolarGrid& 
         "Interpolation: Injection (Radial)",
         // Rank of the index space, Iteration pattern between tiles, Iteration pattern within tiles
         Kokkos::MDRangePolicy<Kokkos::Rank<2, Kokkos::Iterate::Left, Kokkos::Iterate::Left>>( // Iteration policy
-            {smoother_circles, 0}, // Starting point of the index space
-            {nr, ntheta} // Ending point of the index space
+            {coarse_grid.numberSmootherCircles(), 0}, // Starting point of the index space
+            {coarse_grid.nr(), coarse_grid.ntheta()} // Ending point of the index space
             ),
         // Kokkos lambda function to execute for each point in the index space
-        KOKKOS_LAMBDA(int i_r_coarse, int i_theta_coarse) {
+        KOKKOS_LAMBDA(const int i_r_coarse, const int i_theta_coarse) {
             coarseNodeInjection(i_r_coarse, i_theta_coarse, fine_grid, coarse_grid, coarse_result, fine_values);
         });
 
