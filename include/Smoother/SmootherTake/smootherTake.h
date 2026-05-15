1d38502146e6b490842e17da92d457d179e78298
Emily Bourne <emily.bourne@epfl.ch>
2026-05-15 15:55:18 +0200

    nodeBuildTridiagonalSolverMatrices can be static. KOKKOS_CLASS_LAMBDA should be avoided in SmootherTake

--- include/Smoother/SmootherTake/smootherTake.h
+++ include/Smoother/SmootherTake/smootherTake.h
@@ -150,12 +150,12 @@ public:
     // Build all A_sc matrices for circle and radial smoothers.
     void buildTridiagonalSolverMatrices();
     // Build the tridiagonal solver matrices for a specific node (i_r, i_theta)
-    void nodeBuildTridiagonalSolverMatrices(int i_r, int i_theta, const PolarGrid& grid, bool DirBC_Interior,
-                                            const BatchedTridiagonalSolver<double>& circle_tridiagonal_solver,
-                                            const BatchedTridiagonalSolver<double>& radial_tridiagonal_solver,
-                                            ConstVector<double>& arr, ConstVector<double>& att,
-                                            ConstVector<double>& art, ConstVector<double>& detDF,
-                                            ConstVector<double>& coeff_beta) const;
+    static KOKKOS_FUNCTION void
+    nodeBuildTridiagonalSolverMatrices(int i_r, int i_theta, const PolarGrid& grid, bool DirBC_Interior,
+                                       const BatchedTridiagonalSolver<double>& circle_tridiagonal_solver,
+                                       const BatchedTridiagonalSolver<double>& radial_tridiagonal_solver,
+                                       ConstVector<double>& arr, ConstVector<double>& att, ConstVector<double>& art,
+                                       ConstVector<double>& detDF, ConstVector<double>& coeff_beta);
 
     // Build the solver matrix for the interior boundary (i_r = 0) which is non-tridiagonal due to across-origin coupling.
     InnerBoundaryMatrix buildInteriorBoundarySolverMatrix();
