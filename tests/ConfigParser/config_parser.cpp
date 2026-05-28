f12c4f6e760be0cdcb7de9344397acde3075a2a8
BOURNE Emily EPFL <emily.bourne@epfl.ch>
2026-05-28 16:55:18 +0200

    Ensure PolarGrid is passed on correct memory space

--- tests/ConfigParser/config_parser.cpp
+++ tests/ConfigParser/config_parser.cpp
@@ -169,7 +169,7 @@ TEST_P(ConfigParserTest, ParseAllGeometryAndProblemCombinations)
     EXPECT_EQ(parser.cacheDomainGeometry(), cacheDomainGeometry);
 
     // Grid
-    const PolarGrid<Kokkos::HostSpace>& grid = parser.grid();
+    const PolarGrid<DefaultMemorySpace>& grid = parser.grid();
     EXPECT_NE(&grid, nullptr);
     EXPECT_DOUBLE_EQ(grid.radius(0), R0);
     EXPECT_DOUBLE_EQ(grid.radius(grid.nr() - 1), Rmax);
