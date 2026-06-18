05f7b66e51617856299aade84284d3c5ac0129e6
Emily Bourne <emily.bourne@epfl.ch>
2026-06-18 13:20:10 +0200

    Clang format

--- tests/ExtrapolatedSmoother/extrapolated_smoother.cpp
+++ tests/ExtrapolatedSmoother/extrapolated_smoother.cpp
@@ -62,9 +62,9 @@ TEST(ExtrapolatedSmootherTest, extrapolatedSmoother_DirBC_Interior)
     ExtrapolatedSmootherGive smootherGive_operator(level.grid(), level.levelCache(), DirBC_Interior);
     ExtrapolatedSmootherTake smootherTake_operator(level.grid(), level.levelCache(), DirBC_Interior);
 
-    Vector<double> rhs       = generate_random_sample_data(level.grid(), 69);
+    Vector<double> rhs   = generate_random_sample_data(level.grid(), 69);
     Vector<double> start = generate_random_sample_data(level.grid(), 24);
-    Vector<double> temp      = generate_random_sample_data(level.grid(), 8);
+    Vector<double> temp  = generate_random_sample_data(level.grid(), 8);
 
     Vector<double> solution_Give("solution_Give", start.size());
     Kokkos::deep_copy(solution_Give, start);
@@ -122,9 +122,9 @@ TEST(ExtrapolatedSmootherTest, extrapolatedSmoother_AcossOrigin)
     ExtrapolatedSmootherGive smootherGive_operator(level.grid(), level.levelCache(), DirBC_Interior);
     ExtrapolatedSmootherTake smootherTake_operator(level.grid(), level.levelCache(), DirBC_Interior);
 
-    Vector<double> rhs       = generate_random_sample_data(level.grid(), 69);
+    Vector<double> rhs   = generate_random_sample_data(level.grid(), 69);
     Vector<double> start = generate_random_sample_data(level.grid(), 24);
-    Vector<double> temp      = generate_random_sample_data(level.grid(), 8);
+    Vector<double> temp  = generate_random_sample_data(level.grid(), 8);
 
     Vector<double> solution_Give("solution_Give", start.size());
     Kokkos::deep_copy(solution_Give, start);
