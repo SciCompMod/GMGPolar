70eb7c7dd06a15ae6696e0b5082c57e458f9edf2
Thomas Padioleau <thomas.padioleau@cea.fr>
2026-08-25 20:57:10 +0200

    Improve compile time by making benchmarks optional (#319)

--- scripts/tutorial/convergence_order.sh
+++ scripts/tutorial/convergence_order.sh
@@ -5,6 +5,6 @@
 # Get the directory of the script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Set path to the GMGPolar executable
-EXECUTABLE="${SCRIPT_DIR}/../../build/convergence_order"
+EXECUTABLE="${SCRIPT_DIR}/../../build/benchmarks/convergence_order"
 
 "$EXECUTABLE"
--- scripts/tutorial/run.sh
+++ scripts/tutorial/run.sh
@@ -3,7 +3,7 @@
 # Get the directory of the script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Set path to the GMGPolar executable
-GMGPOLAR_EXEC="${SCRIPT_DIR}/../../build/gmgpolar"
+GMGPOLAR_EXEC="${SCRIPT_DIR}/../../build/benchmarks/gmgpolar"
 
 # Verbosity level: 
 # 0 - No output 
