64157b8e5e5779a11dd82688d61f92b96c1646d5
julianlitz <julian.litz@icloud.com>
2026-08-29 22:36:18 +0200

    Showcase tests pass with PCR Solver

--- include/LinearAlgebra/Solvers/tridiagonal_solver.h
+++ include/LinearAlgebra/Solvers/tridiagonal_solver.h
@@ -123,16 +123,18 @@ protected:
 namespace gmgpolar
 {
 
-#if defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP) || defined(KOKKOS_ENABLE_SYCL)
-
-template <typename T>
 using BatchedTridiagonalSolver = BatchedTridiagonalSolverPCR<T>;
 
-#else
+// #if defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP) || defined(KOKKOS_ENABLE_SYCL)
 
-template <typename T>
-using BatchedTridiagonalSolver = BatchedTridiagonalSolverThomas<T>;
+// template <typename T>
+// using BatchedTridiagonalSolver = BatchedTridiagonalSolverPCR<T>;
 
-#endif
+// #else
+
+// template <typename T>
+// using BatchedTridiagonalSolver = BatchedTridiagonalSolverThomas<T>;
+
+// #endif
 
 } // namespace gmgpolar
