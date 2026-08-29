ab868251343c217fd61b5b98cf2ac73655d37996
Julian Litz <91479202+julianlitz@users.noreply.github.com>
2026-08-29 23:24:10 +0200

    Showcase Thomas passes tests

--- include/LinearAlgebra/Solvers/tridiagonal_solver.h
+++ include/LinearAlgebra/Solvers/tridiagonal_solver.h
@@ -123,7 +123,16 @@ protected:
 namespace gmgpolar
 {
 
+#if defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP) || defined(KOKKOS_ENABLE_SYCL)
+
 template <typename T>
 using BatchedTridiagonalSolver = BatchedTridiagonalSolverPCR<T>;
+
+#else
+
+template <typename T>
+using BatchedTridiagonalSolver = BatchedTridiagonalSolverThomas<T>;
+
+#endif
 
 } // namespace gmgpolar
